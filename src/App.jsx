e1e07659f404155228946bb4f838f8a080588c33
beykerDevlopia <beyker@devlopia.com>
2024-06-07 22:27:51 -0400

    Roll back tally script

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  useEffect(() => {
-    // Asegúrate de que Tally.loadEmbeds está disponible
-    if (window.Tally) {
-      window.Tally.loadEmbeds();
-    } else {
-      // Carga el script de Tally si no está disponible
-      const script = document.createElement('script');
-      script.src = 'https://tally.so/widgets/embed.js';
-      script.async = true;
-      script.onload = () => {
-        window.Tally.loadEmbeds();
-      };
-      document.body.appendChild(script);
-    }
-  }, []); // El array vacío asegura que esto solo se ejecute una vez cuando el componente se monta
+  
 
   return (
     <BrowserRouter>
--- src/components/ContactTally.jsx
+++ src/components/ContactTally.jsx
@@ -1,6 +1,21 @@
 import React, { useEffect } from 'react';
 
 const ContactTally = () => {
+  useEffect(() => {
+    // Asegúrate de que Tally.loadEmbeds está disponible
+    if (window.Tally) {
+      window.Tally.loadEmbeds();
+    } else {
+      // Carga el script de Tally si no está disponible
+      const script = document.createElement('script');
+      script.src = 'https://tally.so/widgets/embed.js';
+      script.async = true;
+      script.onload = () => {
+        window.Tally.loadEmbeds();
+      };
+      document.body.appendChild(script);
+    }
+  }, []); // El array vacío asegura que esto solo se ejecute una vez cuando el componente se monta
 
   return (
     <div>
